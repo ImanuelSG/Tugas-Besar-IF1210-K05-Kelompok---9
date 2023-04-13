1db209e5737cd3a62ec07b4024b2777f2ece9f80
unknown <creojr88@gmail.com>
2023-04-13 21:15:41 +0700

    login

--- Login.py
+++ Login.py
@@ -22,7 +22,7 @@ def login() :
         user = input("Username : ")
         password = input("Password : ")
         if isRegistered(user, password):
-            print(f"\n Selamat datang, {user}!")
+            print(f"\nSelamat datang, {user}!")
             print('Masukkan command "help" untuk daftar command yang dapat kamu panggil.')
             isCorrect = True # Ketika True, akan keluar dari loop untuk meminta username dan password
             isLoggedIn = True # Ketika True, user harus melakukan logout agar isLoggedIn menjadi false lalu dapat melakukan login lagi
