efcd05eeeeaed5460d1bab4f10fbc22c32889c4b
unknown <creojr88@gmail.com>
2023-04-14 20:07:28 +0700

    ayam

--- Ayamberkokok.py
+++ Ayamberkokok.py
@@ -18,7 +18,7 @@ def ayamberkokok() :
         print("Selamat, Roro Jonggrang memenangkan permainan!\n")
         print("*Bandung Bondowoso angry noise*\n")
         print("Roro Jonggrang dikutuk jadi candi")
-        sys.exit()
+        exit()
     else : # count >= 100
         print("Yah, Bandung Bondowoso memenangkan permainan")
-        sys.exit()
+        exit()
