f75b3530ddddfba660aab65a087ae81f9432c114
unknown <creojr88@gmail.com>
2023-04-14 20:52:10 +0700

    Rev Ayam

--- Ayamberkokok.py
+++ Ayamberkokok.py
@@ -1,16 +1,9 @@
 import Global
+from HancurkanCandi import banyak_candi
 
 def ayamberkokok() :
     print("Kukuruyuk.. Kukuruyuk..\n")
-    count = 0 # Untuk menghitung banyak candi yang ada
-    inIndex = True # Untuk flag bahwa looping dibawah masih dalam range
-    i = 0
-    while inIndex == True :
-        if Global.candi[i] != "" :
-            count += 1
-            i += 1
-        else : # Datacandi kosong
-            inIndex = False
+    count = banyak_candi(Global.candi)
     print(f"Jumlah candi: {count}\n")
 
     if count < 100 :
