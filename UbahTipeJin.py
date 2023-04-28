7b34595c41e472f47186c3b6a7ebb8cedd4b4b9d
unknown <creojr88@gmail.com>
2023-04-28 14:39:34 +0700

    tambah print

--- UbahTipeJin.py
+++ UbahTipeJin.py
@@ -13,9 +13,11 @@ def UbahTipeJin():
         else :
             show = ["Pembangun","Pengumpul"]
         pilihan = str (input(f'Jin ini bertipe "{show[0]}". Yakin ingin mengubah ke tipe "{show[1]}" (Y/N)? '))
-        if pilihan == 'Y':
+        if pilihan == 'Y' or pilihan == 'y':
             if Global.users[posisi][2] == 'jin_pengumpul':
                 Global.users[posisi][2] = 'jin_pembangun'
             else :
                 Global.users[posisi][2] = 'jin_pengumpul'
             print("Jin telah berhasil diubah.")
+        else :
+            pass
